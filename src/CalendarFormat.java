a01144558a438cb491b370329bba5657c35d1c16
niko saenz <nsaenz2@ggc.edu>
2024-09-12 19:32:35 -0400

    commit

--- src/CalendarFormat.java
+++ src/CalendarFormat.java
@@ -1,14 +1,15 @@
+//demo
 public class CalendarFormat {
     public static void main(String[] args) {
-        String day = "Monday";  // Day of the week
-        int date = 2;           // Day of the month
-        String month = "September";  // Month
-        int year = 2019;        // Year
+        String day = "Monday";
+        int date = 2;
+        String month = "September";
+        int year = 2019;
         System.out.println("Day: " + day);
         System.out.println("Date: " + date);
         System.out.println("Month: " + month);
         System.out.println("Year: " + year);
-        System.out.println("\nAmerican format: " + day + ", " + month + " " + date + ", " + year);
+        System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);
         System.out.println("European format: " + day + " " + date + " " + month + " " + year);
     }
 }
